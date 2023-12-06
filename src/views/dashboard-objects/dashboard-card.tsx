845f3791813328e6f8e6b7b81560e9541593f110
Trevor Busk <buskt@ferris.edu>
2023-12-05 20:34:42 -0500

    Updating centering to dashboard cards

--- src/views/dashboard-objects/dashboard-card.tsx
+++ src/views/dashboard-objects/dashboard-card.tsx
@@ -7,11 +7,11 @@ export default function DashboardCard(props: DashboardCardProps) {
 
     const { title, count } = props;
     return (
-            <div className="card border-left-secondary shadow h-100 py-2">
-                <div className="card-body">
-                    <div className="row no-gutters align-items-center">
-                        <div className="col mr-2">
-                            <div className="text-xs font-weight-bold text-primary text-uppercase mb-1 text-center">
+            <div className="card border-left-secondary shadow h-100 py-2 mx-auto">
+                <div className="card-body text-center">
+                    <div className="row no-gutters">
+                        <div className="col-12">
+                            <div className="text-xs font-weight-bold text-primary text-uppercase mb-1">
                                 {title}
                             </div>
                             <div className="h5 mb-0 font-weight-bold text-gray-800 text-center"> {count}</div>
--- src/views/dashboard-view.tsx
+++ src/views/dashboard-view.tsx
@@ -12,11 +12,11 @@ export default function Dashboard() {
 
             <div className="m-5">
                 <div className="row">
-                    <div className="col-lg-6 col-md-6 col-sm-12 mb-3"><DashboardCard title='Total Users' count={UsersTotal()}/></div>
+                    <div className="col-lg-6 col-md-6 col-sm-12 mb-3 justify-content-center align-items-center"><DashboardCard title='Total Users' count={UsersTotal()}/></div>
                     <div className="col-lg-6 col-md-6 col-sm-12 mb-3"><DashboardCard title='Total Admins' count={AdminTotal()}/></div>
-                    <div className="col-lg-4 col-md-6 col-sm-12 mb-3"><DashboardCard title='New Users (Past 30 Days)' count={NewUsersMonthlyTotal()}/></div>
-                    <div className="col-lg-4 col-md-6 col-sm-12 mb-3"><DashboardCard title='New Users (Past 90 Days)' count={NewUsersTriMonthlyTotal()}/></div>
-                    <div className="col-lg-4 col-md-6 col-sm-12 mb-3"><DashboardCard title='New Users (Past Year)' count={NewUsersYearlyTotal()}/></div>
+                    <div className="col-lg-4 col-md-6 col-sm-12 mb-3"><DashboardCard title='New Users (30 Days)' count={NewUsersMonthlyTotal()}/></div>
+                    <div className="col-lg-4 col-md-6 col-sm-12 mb-3"><DashboardCard title='New Users (90 Days)' count={NewUsersTriMonthlyTotal()}/></div>
+                    <div className="col-lg-4 col-md-6 col-sm-12 mb-3"><DashboardCard title='New Users (Year)' count={NewUsersYearlyTotal()}/></div>
 
                 </div>
             </div>
